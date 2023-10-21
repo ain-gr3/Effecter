387a75611ba8ffaf59819bdf9a2a860eb65a8a12
Ain <ain@AinnoMacBook-Air.local>
2023-10-21 14:48:58 +0900

    Bindable を追加

---
+++ Sources/Effecter/Bindable.swift
@@ -0,0 +1,26 @@
+//
+//  Created by Ain Obara on 2023/10/21.
+//
+
+import SwiftUI
+
+@propertyWrapper
+public struct Bindable<V> {
+
+    private var binding: Binding<V>
+
+    public var wrappedValue: V {
+        didSet {
+            binding.wrappedValue = wrappedValue
+        }
+    }
+
+    public init(_ binding: Binding<V>) {
+        self.binding = binding
+        self.wrappedValue = binding.wrappedValue
+    }
+
+    public static func constant(_ value: V) -> Self {
+        .init(.constant(value))
+    }
+}
